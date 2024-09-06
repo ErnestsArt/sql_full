0c9e77755417aea53b86ec6d92fcb3e04bf70e36
Aivars <example@email.com>
2024-09-06 09:19:35 +0300

    select distinct and select multiple fields

---
+++ select/2.sql
@@ -0,0 +1,15 @@
+-- uzdevums: atlasit datus no tabulas customers
+-- datubazee sql_store
+-- atlasit vardu, uzvardu un punktus
+
+SELECT 
+	first_name,
+    last_name,
+    points,
+    points + 10 AS points_increased
+FROM customers;
+
+SELECT DISTINCT state FROM customers;
+
+
+
